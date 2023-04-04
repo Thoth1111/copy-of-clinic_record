9f04bf4d556142f185815bfd32ca3e060efe2968
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-04 14:10:55 +0300

    Add indexes for all foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -38,20 +38,32 @@ ADD CONSTRAINT FK_patients_medical_histories
 FOREIGN KEY (patient_id)
 REFERENCES patients(id);
 
+--Add indexes to FK 
+ CREATE INDEX patient_id_asc ON medical_histories(patient_id asc);
+
 --Add medical history id column to invoices and set as foreign key
 ALTER TABLE invoices
 ADD COLUMN medical_histories_id INT UNIQUE
 REFERENCES medical_histories(id);
+
+--Add indexes to FK
+CREATE INDEX medical_histories_id_asc ON invoices(medical_histories_id asc);
 
 --Add invoice id column and set as foreign key
 ALTER TABLE invoice_items
 ADD COLUMN invoice_id INT
 REFERENCES invoices(id);
 
+--Add indexes to FK 
+ CREATE INDEX invoice_id_asc ON invoice_items(invoice_id asc);
+
 --Add treatment id column and set as foreign key
 ALTER TABLE invoice_items
 ADD COLUMN treatment_id INT
 REFERENCES treatments(id);
+
+--Add indexes to FK 
+ CREATE INDEX treatment_id_asc ON invoice_items(treatment_id asc);
 
 --create join table from medical histories and treatments table
 CREATE TABLE medical_histories_treatments (
