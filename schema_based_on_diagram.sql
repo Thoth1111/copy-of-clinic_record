600257fc6aebf15ccd9b2428d0cdf1f1906dcb13
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-04-04 14:05:55 +0300

    Add tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,2 +1,33 @@
 -- CREATE NEW DATABASE
 CREATE DATABASE clinic_record;
+
+--CREATE INVOICES TABLE 
+CREATE TABLE invoices (
+    id BIGINT PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP
+);
+
+--CREATE MEDICAL_HISTORIES TABLE 
+CREATE TABLE medical_histories (
+    id BIGINT PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR
+);
+
+--CREATE TREATMENTS TABLE 
+CREATE TABLE treatments (
+    id BIGINT PRIMARY KEY,
+    type VARCHAR,
+    name VARCHAR
+);
+
+-- CREATE INVOICE_ITEMS 
+CREATE TABLE invoice_items (
+    id BIGINT PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL
+);
